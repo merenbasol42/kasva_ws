bcefca2bd7e155d143bba4d6d0dbec8e2a3c4424
ustad <merenbasol@gmail.com>
2025-02-10 12:44:11 +0300

    async seçeneği için iskelet oluşturuldu.

--- src/kasva_obs_avo/kasva_obs_avo/async.py
+++ src/kasva_obs_avo/kasva_obs_avo/async.py
@@ -1,22 +1,65 @@
-
 import rclpy
 from rclpy.node import Node
+
+from sensor_msgs.msg import LaserScan
 
 #
 # Logic
 #
 
-class MyNode(Node):
+class AsyncObsAvo(Node):
     def __init__(self):
-        super().__init__("my_node")
+        super().__init__("kasva_obs_avo")
+
+    #
+    # ROS Methods
+    #
+
+    def scan_cb(self, msg: LaserScan) -> None:
+        pass
+
+    def pub_cmd_vel(self, linear: float, angular: float) -> None:
+        pass
     
+    #
+    # External Access
+    #
+
+    def mainloop(self):
+        while rclpy.ok():
+            if self.detect():
+                # Eğer engel tespit edildiyse
+                if not self.warn():
+                    # Eğer engel ortadan kalkmamışsa
+                    self.avoid()
+            else:
+                pass
+
+    #
+    # 
+    #
+
+    def detect(self) -> bool:
+        pass
+
+    def warn() -> bool:
+        pass
+
+    def avoid(self):
+        pass
+
+    #
+    # Tools
+    #
+    
+
 #
 # Entry Point
 #
 
 def main():
     rclpy.init()
-    node = MyNode()
+    node = AsyncObsAvo()
     try: rclpy.spin(node)
     except: pass
     node.destroy_node()
