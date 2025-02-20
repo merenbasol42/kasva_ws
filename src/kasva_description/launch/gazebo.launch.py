b832a13dcf45301bb7fb6c5ea472732457c5022c
ustad <merenbasol@gmail.com>
2025-02-20 19:45:41 +0300

    simülasyon uygulamaları için robot modeli oluşturuldu. 'description' paketi bu modeli barındırıyor.

---
+++ src/kasva_description/launch/gazebo.launch.py
@@ -0,0 +1,65 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+import xacro
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+from launch import LaunchDescription
+from launch.actions import TimerAction, ExecuteProcess
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+pkg_name = "kasva_description"
+
+def generate_launch_description():
+    # Paketin share dizinini al
+    pkg_share = get_package_share_directory(pkg_name)
+    
+    # Xacro dosyasının tam yolunu oluştur
+    xacro_file = os.path.join(pkg_share, 'urdf', 'gazebo.urdf.xacro')
+    
+    # Xacro dosyasını işle (URDF'e çevir)
+    doc = xacro.process_file(xacro_file)
+    robot_desc = doc.toxml()
+    
+    # İşlenmiş URDF'i geçici bir dosyaya yaz
+    tmp_urdf_file = '/tmp/kasva_robot.urdf'
+    with open(tmp_urdf_file, 'w') as f:
+        f.write(robot_desc)
+    
+    
+    xml_launch_file = PathJoinSubstitution([
+        FindPackageShare(pkg_name),
+        "launch",
+        "gazebo.launch.xml"
+    ])
+    
+    return LaunchDescription([
+        # 1. Ignition Gazebo (Fortress)'u varsayılan world ile başlat
+        ExecuteProcess(
+            cmd=['ign', 'gazebo', 'empty.sdf', '-v', '4'],
+            output='screen'
+        ),
+        IncludeLaunchDescription(
+            AnyLaunchDescriptionSource(
+                xml_launch_file
+            )
+        ),
+        # 4. 10 saniye sonra Ignition servisi ile URDF modelini spawn et
+        TimerAction(
+            period=10.0,  # Gazebo'nun tamamen açılması için bekleme süresi
+            actions=[
+                ExecuteProcess(
+                    cmd=[
+                        'ign', 'service', '-s', '/world/empty/create',
+                        '--reqtype', 'ignition.msgs.EntityFactory',
+                        '--reptype', 'ignition.msgs.Boolean',
+                        '--timeout', '1000',
+                        '--req', f'sdf_filename: "{tmp_urdf_file}", name: "custom_robot"'
+                    ],
+                    output='screen'
+                )
+            ]
+        )
+    ])
