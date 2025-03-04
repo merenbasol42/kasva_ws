2f36452f3445ca1b4256a04d67ad55ea0bc0e4fd
ustad <merenbasol@gmail.com>
2025-03-05 00:06:39 +0300

    şerit takip için yeni parkur eklendi ve world, model dosyaları düzenlendi.

--- src/kasva_simulate/launch/line_follow_t1.launch.py
+++ src/kasva_simulate/launch/line_follow_t1.launch.py
@@ -1,6 +1,5 @@
 import os
 from ament_index_python.packages import get_package_share_directory
-import xacro
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, SetEnvironmentVariable
 from launch.actions import IncludeLaunchDescription
@@ -38,7 +37,7 @@ def generate_launch_description():
     world_path = os.path.join(
         pkg_share,
         "worlds",
-        "line_follow.sdf"
+        "line_follow_t1.sdf"
     )
     
     rviz_cfg_path = os.path.join(
